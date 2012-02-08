0618795c16960dad6b16eec9575fb6cd0ecf09b7
Louis Schnellbach <xleek.okz@gmail.com>
2012-02-08 16:54:42 +0100

    Added functions.h

---
+++ Client/TP1/functions.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+int createUDPSocket(int port) {
+}
