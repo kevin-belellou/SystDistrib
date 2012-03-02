8c35c27ecb12f96aa77bb6d924760dfeea67d88c
Belellou Kevin <gund38@gmail.com>
2012-03-02 18:38:28 +0100

    Add functions.h and it works with the TP1's server but not with the TP1's client, I think I know why but don't know how to resolve it

--- TP1/client.c
+++ TP1/client.c
@@ -8,67 +8,97 @@
 #include <netinet/in.h>
 #include <errno.h>
 
+#include "../functions.h"
+
 #define TAILLEBUF 20
 
-int main(int argc, char** argv) {
-	if(argc != 3) {
+int main(int argc, char** argv)
+{
+	if(argc != 3)
+     {
 		printf("Wrong number of argument\n");
 		return -1;
 	}
+
      // identifiant de la machine serveur
      struct hostent *serveur_host;
+
      // adresse de la socket coté serveur
      static struct sockaddr_in addr_serveur;
+
      // taille de l'addresse socket
      socklen_t lg;
+
      // descripteur de la socket locale
      int sock;
+
+     // Création de la socket et bind via functions.h
+     sock = creerSocketUDP(atoi(argv[2]));
+     if (sock == -1)
+     {
+          printf("fuck\n");
+          exit(-1);
+     }
+
      // chaine à envoyer
      char *msg = "bonjour";
+
      // buffer de réception
      char buffer[TAILLEBUF];
+
      // chaine reçue en réponse
      char *reponse;
+
      // nombre d'octets lus ou envoyés
      int nb_octets;
-     // création d'une socket UDP
-     sock = socket(AF_INET, SOCK_DGRAM, 0);
-     if (sock == -1) {
-          perror("erreur création socket");
-          exit(1);
-     }
+
+//     // création d'une socket UDP
+//     sock = socket(AF_INET, SOCK_DGRAM, 0);
+//     if (sock == -1)
+//     {
+//          perror("erreur création socket");
+//          exit(1);
+//     }
+
      // récupération identifiant du serveur
      serveur_host = gethostbyname(argv[1]);
-     if (serveur_host==NULL) {
+     if (serveur_host == NULL)
+     {
           perror("erreur adresse serveur");
           exit(1);
      }
-     // création adresse socket destinatrice
-     bzero(&addr_serveur, sizeof(struct sockaddr_in));
-     addr_serveur.sin_family = AF_INET;
-     addr_serveur.sin_port = htons(atoi(argv[2]));
-     memcpy(&addr_serveur.sin_addr.s_addr,
-            serveur_host -> h_addr, serveur_host -> h_length);
+
+//     // création adresse socket destinatrice
+//     bzero(&addr_serveur, sizeof(struct sockaddr_in));
+//     addr_serveur.sin_family = AF_INET;
+//     addr_serveur.sin_port = htons(atoi(argv[2]));
+//     memcpy(&addr_serveur.sin_addr.s_addr, serveur_host->h_addr, serveur_host->h_length);
+
      // on envoie le message "bonjour" au serveur
      lg = sizeof(struct sockaddr_in);
-     nb_octets = sendto(sock, msg, strlen(msg)+1, 0,
-                        (struct sockaddr*)&addr_serveur, lg);
-     if (nb_octets == -1) {
+     nb_octets = sendto(sock, msg, strlen(msg)+1, 0, (struct sockaddr*)&addr_serveur, lg);
+     if (nb_octets == -1)
+     {
           perror("erreur envoi message");
           exit(1);
      }
+
      printf("paquet envoyé, nb_octets = %d\n",nb_octets);
+
      // on attend la réponse du serveur
-     nb_octets = recvfrom(sock, buffer, TAILLEBUF, 0,
-                          (struct sockaddr*)&addr_serveur, &lg);
-     if (nb_octets == -1) {
+     nb_octets = recvfrom(sock, buffer, TAILLEBUF, 0, (struct sockaddr*)&addr_serveur, &lg);
+     if (nb_octets == -1)
+     {
           perror("erreur réponse serveur");
           exit(1);
      }
+
      reponse = (char *)malloc(nb_octets * sizeof(char));
      memcpy(reponse, buffer, nb_octets);
      printf("reponse recue du serveur : %s\n",reponse);
+
      // on ferme la socket
      close(sock);
+
 	return 0;
 }
---
+++ functions.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+int creerSocketUDP(int port)
+{
+     int sock;
+     static struct sockaddr_in addr;
+
+     // création d'une socket UDP
+     sock = socket(AF_INET, SOCK_DGRAM, 0);
+     if (sock == -1)
+     {
+          //perror("erreur création socket");
+          return -1;
+     }
+
+     // liaison de la socket sur le port local passé en paramètre
+     bzero(&addr, sizeof(struct sockaddr_in));
+     addr.sin_family = AF_INET;
+     addr.sin_port = htons(port);
+     addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+     if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+     {
+          //perror("erreur bind");
+          return -1;
+     }
+
+     return sock;
+}
